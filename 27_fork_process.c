a8073782c2f0b6a9285af786ea42b6db07f91925
Jalpa Nakum <jalpa.nakum1996@gmail.com>
2026-09-24 16:17:27 +0530

    Add fork process example
    Implement a simple fork process example with PID output.

---
+++ 27_fork_process.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fork failed.\n");
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        printf("Child process\n");
+        printf("Child PID: %d\n", getpid());
+    }
+    else
+    {
+        printf("Parent process\n");
+        printf("Parent PID: %d\n", getpid());
+        printf("Child PID: %d\n", pid);
+    }
+
+    return 0;
+}
