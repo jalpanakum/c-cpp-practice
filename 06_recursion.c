5be49479d72bfd997b9bf1d3a0c85d7e59e4dac6
Jalpa Nakum <jalpa.nakum1996@gmail.com>
2026-09-24 11:28:59 +0530

    Add recursion example

---
+++ 06_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+int main(void)
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    }
+    else
+    {
+        printf("Factorial of %d = %d\n", n, factorial(n));
+    }
+
+    return 0;
+}
