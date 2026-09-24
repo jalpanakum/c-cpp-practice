c712050a92febd89bf15940fb8daf49207e27484
Jalpa Nakum <jalpa.nakum1996@gmail.com>
2026-09-24 16:45:53 +0530

    Add TCP echo client server example

--- 44_tcp_echo_server.c
+++ 44_tcp_echo_server.c
@@ -9,54 +9,53 @@
 
 int main(void)
 {
-    int client_fd;
+    int server_fd, client_fd;
     struct sockaddr_in server_address;
     char buffer[BUFFER_SIZE];
 
-    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (client_fd < 0)
+    if (server_fd < 0)
     {
         perror("socket");
         return 1;
     }
 
     server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1",
-                  &server_address.sin_addr) <= 0)
+    if (bind(server_fd,
+             (struct sockaddr *)&server_address,
+             sizeof(server_address)) < 0)
     {
-        perror("inet_pton");
-        close(client_fd);
+        perror("bind");
+        close(server_fd);
         return 1;
     }
 
-    if (connect(client_fd,
-                (struct sockaddr *)&server_address,
-                sizeof(server_address)) < 0)
+    if (listen(server_fd, 5) < 0)
     {
-        perror("connect");
-        close(client_fd);
+        perror("listen");
+        close(server_fd);
         return 1;
     }
 
-    printf("Connected to server.\n");
+    printf("Server waiting for connection...\n");
+
+    client_fd = accept(server_fd, NULL, NULL);
+
+    if (client_fd < 0)
+    {
+        perror("accept");
+        close(server_fd);
+        return 1;
+    }
+
+    printf("Client connected.\n");
 
     while (1)
     {
-        printf("Enter message: ");
-        fgets(buffer, sizeof(buffer), stdin);
-
-        buffer[strcspn(buffer, "\n")] = '\0';
-
-        send(client_fd, buffer, strlen(buffer), 0);
-
-        if (strcmp(buffer, "exit") == 0)
-        {
-            break;
-        }
-
         memset(buffer, 0, sizeof(buffer));
 
         int bytes_received = recv(client_fd,
@@ -66,16 +65,24 @@ int main(void)
 
         if (bytes_received <= 0)
         {
-            printf("Server disconnected.\n");
+            printf("Client disconnected.\n");
             break;
         }
 
         buffer[bytes_received] = '\0';
 
-        printf("Server echoed: %s\n", buffer);
+        printf("Client: %s\n", buffer);
+
+        if (strcmp(buffer, "exit") == 0)
+        {
+            break;
+        }
+
+        send(client_fd, buffer, bytes_received, 0);
     }
 
     close(client_fd);
+    close(server_fd);
 
     return 0;
 }
